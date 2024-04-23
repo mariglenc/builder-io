15fa1c3fe467403e0406f26a46142bd00c014cc5
Mariglen <mcullhaj43@gmail.com>
2024-04-24 00:16:25 +0200

    small fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <p>this is a paeg builder test</p>
+        <p>This is a page builder for nextJs</p>
       </main>
     </>
   );
